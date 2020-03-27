33c99dc74021a5901c91328328801c3ba45cfa1c
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-27 14:19:15 +0100

    add sfx to enemys death

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+
+    [SerializeField] GameObject deathFx;
+    [SerializeField] Transform parent;
+
+    private void Start() {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider() {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other) {
+        GameObject fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
